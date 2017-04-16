ade73d73726be4b8c337d3f5084c53d1b29e3e29
wikku <wikku9@gmail.com>
2017-04-16 17:37:08 +0200

    Install vim-plug

--- setup.sh
+++ setup.sh
@@ -2,10 +2,14 @@
 # This script installs dotfiles
 
 shopt -s dotglob # Make hidden files visible
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 files=".zshrc .config/nvim/init.vim"
-mkdir ~/dotfiles_old
+mkdir -p ~/dotfiles_old
 for file in $files; do
-	echo "Moving ~/$file to ~/dotfiles_old/"
+	echo "Moving ~/$file (if exists) to ~/dotfiles_old/"
 	mv ~/$file ~/dotfiles_old/
 	echo "Creating symlink"
 	ln -s $(pwd)/$file ~/$file
